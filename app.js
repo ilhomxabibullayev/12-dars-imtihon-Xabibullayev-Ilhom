ba5359303f98211550f7c910d23dbe64fcdbf628
ilhomxabibullayev <ilhomxabibullayev2004@gmail.com>
2024-09-30 17:37:56 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,86 @@
+// 6 masala
+
+let findsmallesting = [78, 56, 232, 12, 8];
+
+let max = findsmallesting[0];
+
+let min = findsmallesting[0];
+
+for (let i = 0; i < findsmallesting.length; i++) {
+    if (findsmallesting[i] > max) {
+        max = findsmallesting
+    }
+    else if (findsmallesting[i] < min) {
+        min = findsmallesting[i]
+    }
+}
+
+console.log(max, min);
+
+let findsmallesting2 = [78, 56, 232, 12, 18];
+
+let max2 = findsmallesting2[0];
+
+let min2 = findsmallesting2[0];
+
+for (let i = 0; i < findsmallesting2.length; i++) {
+    if (findsmallesting2[i] > max2) {
+        max2 = findsmallesting2
+    }
+    else if (findsmallesting2[i] < min2) {
+        min2 = findsmallesting2[i]
+    }
+}
+
+console.log(max2, min2);
+
+let findsmallesting3 = [78, 56, 232, 412, 228];
+
+let max3 = findsmallesting3[0];
+
+let min3 = findsmallesting3[0];
+
+for (let i = 0; i < findsmallesting3.length; i++) {
+    if (findsmallesting3[i] > max3) {
+        max3 = findsmallesting3
+    }
+    else if (findsmallesting3[i] < min3) {
+        min3 = findsmallesting3[i]
+    }
+}
+
+console.log(max3, min3);
+
+let findsmallesting4 = [78, 56, 232, 12, 0];
+
+let max4 = findsmallesting4[0];
+
+let min4 = findsmallesting4[0];
+
+for (let i = 0; i < findsmallesting4.length; i++) {
+    if (findsmallesting4[i] > max4) {
+        max4 = findsmallesting4
+    }
+    else if (findsmallesting4[i] < min4) {
+        min4 = findsmallesting4[i]
+    }
+}
+
+console.log(max4, min4);
+
+let findsmallesting5 = [1, 56, 232, 12, 8];
+
+let max5 = findsmallesting4[0];
+
+let min5 = findsmallesting4[0];
+
+for (let i = 0; i < findsmallesting5.length; i++) {
+    if (findsmallesting5[i] > max5) {
+        max5 = findsmallesting5
+    }
+    else if (findsmallesting5[i] < min5) {
+        min5 = findsmallesting5[i]
+    }
+}
+
+console.log(max5, min5);
